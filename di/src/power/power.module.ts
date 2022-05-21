81ce07afd37a57ccd57903562930c1e4c8490757
Tichalo <ticha.muma@chatdesk.com>
2022-05-21 06:55:37 +0100

    base fiolders

--- di/src/power/power.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PowerService } from './power.service';
-
-@Module({
-  providers: [PowerService], //accessible only inside the module. Denotes classes and their dependencies
-  exports: [PowerService] //make this available to other modules
-})
-export class PowerModule {}
